cedbedf85a9b50436135eb3e48aa4a69e8704736
searchivarius <leo@boytsov.info>
2013-12-24 23:37:26 -0500

    New post.

---
+++ 2013/12/24/cmn.h
@@ -0,0 +1,27 @@
+#ifndef CMN_H
+#define CMN_H
+
+#include <stdexcept>
+#include <iostream>
+
+#ifdef  __INTEL_COMPILER
+#include "mkl.h"
+#endif
+
+#include "../ztimer.h"
+
+inline void SetHighAccuracy() {
+#ifdef  __INTEL_COMPILER
+    vmlSetMode(VML_HA);
+    std::cout << "Set high-accuracy mode." << std::endl;
+#endif
+}
+
+inline void SetLowAccuracy() {
+#ifdef  __INTEL_COMPILER
+    vmlSetMode(VML_LA);
+    std::cout << "Set low-accuracy mode." << std::endl;
+#endif
+}
+
+#endif
