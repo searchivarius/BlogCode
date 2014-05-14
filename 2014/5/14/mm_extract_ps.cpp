bc05c3da3e0696741efae4e34b7dee494d9c7ff4
searchivarius <leo@boytsov.info>
2014-05-14 00:19:20 -0400

    _mm_extract_ps woes

---
+++ 2014/5/14/mm_extract_ps.cpp
@@ -0,0 +1,29 @@
+#ifdef __SSE4_2__
+#include <immintrin.h>
+#include <smmintrin.h>
+#include <tmmintrin.h>
+#else
+#error "SSE 4.2 is required!"
+#endif
+
+#include <iostream>
+
+ /*
+ * Based on explanations/suggestions from here
+ * http://stackoverflow.com/questions/5526658/intel-sse-why-does-mm-extract-ps-return-int-instead-of-float
+ */
+
+#define MM_EXTRACT_FLOAT(v,i) _mm_cvtss_f32(_mm_shuffle_ps(v, v, _MM_SHUFFLE(0, 0, 0, i)))
+
+using namespace std;
+
+int main(int,char**) {
+  __m128 v = _mm_set_ps(0.4, 0.3, 0.2, 0.1);
+
+  cout << MM_EXTRACT_FLOAT(v, 0) << endl;
+  cout << MM_EXTRACT_FLOAT(v, 1) << endl;
+  cout << MM_EXTRACT_FLOAT(v, 2) << endl;
+  cout << MM_EXTRACT_FLOAT(v, 3) << endl;
+
+  return 0;
+}
