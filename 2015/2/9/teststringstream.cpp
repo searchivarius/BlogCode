4bdf311f2d26be71cb806ab2a6587cf70226a4de
searchivarius <leo@boytsov.info>
2015-02-09 23:39:23 -0500

    new blog post

---
+++ 2015/2/9/teststringstream.cpp
@@ -0,0 +1,70 @@
+/**
+ * This code is released under the
+ * Apache License Version 2.0 http://www.apache.org/licenses/.
+ *
+ * (c) Leonid Boytsov, http://boytsov.info
+ */
+
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+
+#include "../../../2014/ztimer.h"
+
+using namespace std;
+
+void test1(int N, int rep) {
+  WallClockTimer timer;
+
+  uint64_t total = 0;
+
+  uint64_t sum = 0;
+
+  for (int j = 0; j < rep; ++j) {
+
+    timer.reset();
+
+
+    for (int i = 0; i < N; i++) {
+      stringstream str;
+      sum += reinterpret_cast<size_t>(str.str().c_str());
+    }
+
+    total += timer.split();
+  }
+
+  cout << "Ignore: " << sum << endl;
+  cout << " total # of construct/destruct: " << rep * N << ", time " <<  total / 1e3 << " ms" << " Construct/destruct/ per sec: " << (rep * N * 1e6 / total ) << endl;
+}
+
+void test2(int N, int rep) {
+  WallClockTimer timer;
+
+  uint64_t total = 0;
+
+  uint64_t sum = 0;
+
+  for (int j = 0; j < rep; ++j) {
+
+    timer.reset();
+
+
+    for (int i = 0; i < N; i++) {
+      stringstream str;
+      //str << i << " " << j; 
+      sum += reinterpret_cast<size_t>(str.str().c_str());
+    }
+
+    total += timer.split();
+  }
+
+  cout << "Ignore: " << sum << endl;
+  cout << " total # of construct/destruct/proc: " << rep * N << ", time " <<  total / 1e3 << " ms" << " Construct/destruct/proc per sec: " << (rep * N * 1e6 / total ) << endl;
+}
+
+int main() {
+  test1(1024*1024, 10);
+  test2(1024*1024, 10);
+}
