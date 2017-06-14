4806e0811f28678f7a0c0b4f35b3cb9bf75d728a
searchivairus <leo@boytsov.info>
2017-06-14 15:41:11 -0400

    minor change

--- 2017/6/14/const_ptr_vs_const_ref.cc
+++ 2017/6/14/const_ptr_vs_const_ref.cc
@@ -4,7 +4,8 @@ using namespace std;
 
 int main() {
   int x=3;
-  int  const &rx  = x;
+  int  const &rx  = x; // Seems like a constant reference, but it's not!
+  //int  & const rx  = x; // Compile error: a true constant reference, which is illegal
   int* const px   = &x;
   //rx = 33; // Compile error, can't assign through a constant ref.
   //px++;    // Compile error, because the pointer value is constant!
