fcb3b07027bd7696f8fd6702c6a4749fc4e152f0
searchivarius <leo@boytsov.info>
2022-08-11 18:29:16 -0400

    a test for HF accelerate.

---
+++ 2022/test_accelerate/README.md
@@ -0,0 +1,14 @@
+## Testing multi-GPU acceleration.
+
+This code was created to test the effectiveness of the Huggingface library [Accelerate](https://github.com/huggingface/accelerate).
+This was motivated by a somewhat anecdotal observation that synchronous SGD does not scale with the number of GPUs when these GPUs are "sitting" on a slow interconnect (PCI) express rather than being connected using a fast interconnect system such as NVLink.
+
+
+It was intended to run as an end-to-end example (run the script (run_main.sh)[run_main.sh], which does everything including installing the environment and running tests. Some manual intervention might be required, in particular, (by specifying a different CUDA version or changing otherwise an installation script)[run_sub.sh].
+Likewise, you may want to change the number of training examples, or run these experiments for several seeds.
+
+
+## Basic requirements
+
+1. Linux system with multiple GPUs and CUDA installed.
+2. conda
---
+++ 2022/test_accelerate/run_sub.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+conda activate test_accelerate
+
+export CUDA_VERSION=11.3
+conda install pytorch cudatoolkit=$CUDA_VERSION -c pytorch -y
+
+pip install "transformers==4.15.0" 
+pip install "datasets==1.14.0" 
+pip install "wrapt" "flatbuffers"
+# This must go last or else we install a very old incompatible version of the hub
+pip install "huggingface_hub==0.4.0"
+#pip install "accelerate==0.9.0" 
+pip install "accelerate"
+
+echo "Installation finished!"
+
+BERT_MODEL=bert-large-uncased
+MAX_TRAIN_SAMPLES=5000
+MAX_EVAL_SAMPLES=10000
+BATCH_SIZE=8
+OUTPUT_PREF=output_res
+
+rm -f run_qa_no_trainer.py* utils_qa.py*
+
+wget https://raw.githubusercontent.com/huggingface/transformers/v4.15.0/examples/pytorch/question-answering/run_qa_no_trainer.py
+wget https://raw.githubusercontent.com/huggingface/transformers/v4.15.0/examples/pytorch/question-answering/utils_qa.py
+
+# The first run is on a single GPU and without gradient accumulation
+python run_qa_no_trainer.py \
+  --max_train_samples $MAX_TRAIN_SAMPLES \
+  --max_eval_samples $MAX_EVAL_SAMPLES \
+  --model_name_or_path bert-large-uncased \
+  --per_device_train_batch_size $BATCH_SIZE \
+  --gradient_accumulation_steps 1 \
+  --learning_rate 3e-5 \
+  --dataset_name squad \
+  --max_seq_length 384 \
+  --doc_stride 128  \
+  --output_dir ${OUTPUT_PREF}_1gpu 2>&1|tee ${OUTPUT_PREF}_1gpu/run.log
+
+for grad_accum_steps in 1 2 4 8 16 ; do
+  accelerate launch  run_qa_no_trainer.py \
+  --max_train_samples $MAX_TRAIN_SAMPLES \
+  --max_eval_samples $MAX_EVAL_SAMPLES \
+  --model_name_or_path bert-large-uncased \
+  --per_device_train_batch_size $BATCH_SIZE \
+  --gradient_accumulation_steps $grad_accum_steps \
+  --learning_rate 3e-5 \
+  --dataset_name squad \
+  --max_seq_length 384 \
+  --doc_stride 128  \
+  --output_dir ${OUTPUT_PREF}_accum_steps_${grad_accum_steps} 2>&1|tee ${OUTPUT_PREF}_accum_steps_${grad_accum_steps}/run.log
+done
